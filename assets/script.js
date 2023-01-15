96f4cb632930659765ff0e4fa76fbd2bb0c81ef1
KlosheKopa <richardsa@my.lanecc.edu>
2023-01-14 18:30:23 -0800

    Added basic javascript loop

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+console.log("Let's study:");
+
+for(i = 0; i < topics.length; i++){
+    console.log(topics[i]);
+}
+
+/*
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");s
+} else {
+  console.log('Please try again!');
+}*/
